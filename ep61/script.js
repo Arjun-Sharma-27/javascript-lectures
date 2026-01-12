bde2b2efad095caef9be49e5c51494b8ad599da8
Arjun Sharma <arjun131315.com>
2026-01-13 00:40:38 +0530

    DIsplay messagd on card div using addeventlistener

--- ep61/script.js
+++ ep61/script.js
@@ -1,11 +1,17 @@
 const hi=document.querySelector("h1")
 // hi.onclick= sayHi
 hi.addEventListener('click',sayHi)
+hi.addEventListener('click',function(){
+    console.log("sayhiiii");
+})
 
 function sayHi(){
     console.log("HI")
 
 }
+const helloCard=document.querySelector('.card')
+helloCard.addEventListener('click',hello)
+
  function hello(){
       console.log("hi there");
  }
