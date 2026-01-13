17fb1c728d57f5f97f0cd4106aac049f65fe5ea6
Arjun Sharma <arjun131315.com>
2026-01-13 17:12:43 +0530

    applied operation to the username

--- ep62/script.js
+++ ep62/script.js
@@ -1,11 +1,16 @@
 const inputUsername=document.querySelector('#username')
+const paragraph=document.querySelector('p')
 // inputUsername.addEventListener('click', ()=>{
 //     console.log('input clicked');
 // })
 // inputUsername.addEventListener('dblclick', ()=>{
 //     console.log(' double input clicked');
 // })
+let inputValue
 inputUsername.addEventListener('input', (e)=>{
-    console.log(e);
+    console.log(e.target.value);
+    inputValue=e.target.value
+
+    paragraph.innerText=e.target.value
     console.log(' double input clicked');
 })
