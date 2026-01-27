1bd12332179c9a60be5a274915d6e86808c5e5c2
Arjun Sharma <arjun131315.com>
2026-01-28 01:22:58 +0530

    add dog fetch api

--- Ep69/script.js
+++ Ep69/script.js
@@ -1,13 +1,10 @@
- fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json))
+const image=document.querySelector('img')
+
+ fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(json =>{
+        console.log("got this");
+         console.log(json.message)
+         image.src=json.message
+      
+      })
