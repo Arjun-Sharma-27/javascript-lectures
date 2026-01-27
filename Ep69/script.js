74a927e57e39c2501b953b51e80392bc1d9dea71
Arjun Sharma <arjun131315.com>
2026-01-28 00:21:36 +0530

    add post method request to js file

--- Ep69/script.js
+++ Ep69/script.js
@@ -1,9 +1,13 @@
- let userData
-
- fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json =>{
-        console.log("got this");
-         console.log(json)
-         userData=json
-      })
+ fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json))
