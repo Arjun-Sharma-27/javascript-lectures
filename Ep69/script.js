aa6d5cb977ed1d459106bc0cee60c3ed3fee5ecd
Arjun Sharma <arjun131315.com>
2026-01-28 01:35:49 +0530

    add div to button

--- Ep69/script.js
+++ Ep69/script.js
@@ -1,10 +1,10 @@
-const image=document.querySelector('img')
+const image = document.querySelector('img')
+const button = document.querySelector('button')
 
- fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(json =>{
-        console.log("got this");
-         console.log(json.message)
-         image.src=json.message
-      
-      })
+button.addEventListener('click', () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(json => {
+    image.src = json.message
+  })
+})
