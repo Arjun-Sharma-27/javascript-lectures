df5e038e9e4ac57cb52a7c5554f6bf6315f8aeb1
Arjun Sharma <arjun131315.com>
2026-02-02 00:11:40 +0530

    add onload event listener

--- Ep70/script.js
+++ Ep70/script.js
@@ -9,14 +9,17 @@ const button = document.querySelector('button')
 //   })
 // })
 button.addEventListener('click', () => {
-    const xhr = new XMLHttpRequest
-    xhr.responseType = 'json'
-    xhr.addEventListener('load', () => {
-        image.src = xhr.response.message
-        console.log(xhr.response);
-        console.log(xhr);
-
-    })
-    xhr.open('GET', "https://dog.ceo/api/breeds/image/random")
-    xhr.send()
+  const xhr= new XMLHttpRequest
+  xhr.responseType='json'
+//   xhr.addEventListener('load',()=>{
+//     image.src=xhr.response.message
+//   console.log(xhr.response);
+//   console.log(xhr);
+xhr.onload=()=>{
+    image.src=xhr.response.message
+ console.log(xhr.response);
+}
+  
+  xhr.open('GET',"https://dog.ceo/api/breeds/image/random")
+  xhr.send()
 })
