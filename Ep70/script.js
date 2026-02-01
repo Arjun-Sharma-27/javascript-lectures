aef7537b4000be5240d4393e85931cf5898ea4b6
Arjun Sharma <arjun131315.com>
2026-02-01 23:49:46 +0530

    tested xhr request

--- Ep70/script.js
+++ Ep70/script.js
@@ -10,6 +10,10 @@ const button = document.querySelector('button')
 // })
 button.addEventListener('click', () => {
   const xhr= new XMLHttpRequest
+  console.log(xhr);
+  xhr.addEventListener('load',()=>{
+  console.log(xhr);
+  })
   xhr.open('GET',"https://dog.ceo/api/breeds/image/random")
   xhr.send()
 })
