4af59822945e08e169c9161a7b9b0300facd6d38
Arjun Sharma <arjun131315.com>
2026-02-08 22:57:28 +0530

    changes made to script

--- Ep71/script.js
+++ Ep71/script.js
@@ -1,36 +1,28 @@
 const image = document.querySelector('img')
 const button = document.querySelector('button')
 
-// button.addEventListener('click', () => {
-//   fetch('https://dog.ceo/api/breeds/image/random')
-//   .then(response => response.json())
-//   .then(json => {
-//     image.src = json.message 
-//   })
-// })
+
 button.addEventListener('click', () => {
   const xhr= new XMLHttpRequest
   xhr.responseType='json'
-//   xhr.addEventListener('load',()=>{
-//     image.src=xhr.response.message
-//   console.log(xhr.response);
-//   console.log(xhr);
-xhr.onload=()=>{
+  xhr.addEventListener('load',()=>{
     image.src=xhr.response.message
- console.log(xhr.response);
-}
+  console.log(xhr.response);
+  console.log(xhr);
+
   
   xhr.open('GET',"https://dog.ceo/api/breeds/image/random")
   xhr.send()
 })
+})
 // setTimeout(()=>{
 // console.log('hi');
 // },4000)
-const blockinbutton= document.querySelector(".blockin-btn")
-blockinbutton.addEventListener('click',()=>{
-const startTime= Date.now()
-let currentTime= startTime
-while(startTime+4000> currentTime){
-  currentTime=Date.now()
-}
-})
+// const blockinbutton= document.querySelector(".blockin-btn")
+// blockinbutton.addEventListener('click',()=>{
+// const startTime= Date.now()
+// let currentTime= startTime
+// while(startTime+4000> currentTime){
+//   currentTime=Date.now()
+// }
+// })
