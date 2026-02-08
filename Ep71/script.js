d32bf4fb91ab32004a1095bc806a2bc5fa9b3b65
Arjun Sharma <arjun131315.com>
2026-02-08 20:25:55 +0530

    Add perevious code to file

---
+++ Ep71/script.js
@@ -0,0 +1,25 @@
+const image = document.querySelector('img')
+const button = document.querySelector('button')
+
+// button.addEventListener('click', () => {
+//   fetch('https://dog.ceo/api/breeds/image/random')
+//   .then(response => response.json())
+//   .then(json => {
+//     image.src = json.message
+//   })
+// })
+button.addEventListener('click', () => {
+  const xhr= new XMLHttpRequest
+  xhr.responseType='json'
+//   xhr.addEventListener('load',()=>{
+//     image.src=xhr.response.message
+//   console.log(xhr.response);
+//   console.log(xhr);
+xhr.onload=()=>{
+    image.src=xhr.response.message
+ console.log(xhr.response);
+}
+  
+  xhr.open('GET',"https://dog.ceo/api/breeds/image/random")
+  xhr.send()
+})
